872233d6b0add99b8c3e3dd0510b1f24df82f263
APrem-7 <ap7leo@gmail.com>
2025-12-15 16:06:35 +0530

    Add: implement sign-up form with validation and error handling same as the signIn Form

--- ai-saas/src/modules/auth/ui/views/sign-up-view.tsx
+++ ai-saas/src/modules/auth/ui/views/sign-up-view.tsx
@@ -1,5 +1,233 @@
 "use client";
-import { Card } from "@/components/ui/card";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+
+import { z } from "zod";
+import { OctagonAlertIcon } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+
+import { authClient } from "@/lib/auth-client";
+import {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from "@/components/ui/form";
+
+//z is similar to dataclass/pydantic but for TypeScript
+const formSchema = z
+  .object({
+    name: z.string().min(1, { message: "Name is required" }),
+    email: z.string().email(),
+    password: z.string().min(1, { message: "Password is required" }),
+    confirmPassword: z.string().min(1, { message: "Password is required" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
+
 export const SignUpView = () => {
-  return <Card>Sign Up Page</Card>;
+  const router = useRouter();
+  const [pending, setPending] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    setError(null);
+    setPending(true);
+
+    await authClient.signUp.email(
+      {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      },
+      {
+        onSuccess: () => {
+          setPending(false);
+          router.push("/");
+        },
+        onError: (error) => {
+          setError(error?.error?.message);
+          setPending(false);
+        },
+      }
+    );
+  };
+  return (
+    <div className="flex flex-col gap-6">
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 md:p-8">
+              <div className="flex flex-col gap-6 ">
+                <div className="flex flex-col items-center  text-center">
+                  <h1 className="text-2xl font-bold">
+                    Let&apos;s get started!{" "}
+                  </h1>
+                  <p className="text-muted-foreground text-balance">
+                    Create your account
+                  </p>
+                </div>
+
+                <div className="grid gap-3 ">
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>name</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="name"
+                            placeholder="John Doe"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  ></FormField>
+                </div>
+
+                <div className="grid gap-3 ">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>email</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="email"
+                            placeholder="example@email.com"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  ></FormField>
+                </div>
+                <div className="grid gap-3 ">
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>password</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="must have atleast 6 letters"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  ></FormField>
+                </div>
+                <div className="grid gap-3 ">
+                  <FormField
+                    control={form.control}
+                    name="confirmPassword"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Confirm Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="confirmPassword"
+                            placeholder="must have atleast 6 letters"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  ></FormField>
+                </div>
+                {!!error && (
+                  <Alert className="bg-destructive/10 border-none">
+                    <OctagonAlertIcon className="h-4 w-4 !text-destructive " />
+                    <AlertTitle>{error}</AlertTitle>
+                  </Alert>
+                )}
+                <Button disabled={pending} type="submit" className="w-full">
+                  Sign Up
+                </Button>
+                <div
+                  className="after:border-border relative text-center text-sm after:absolute after:inset-0 
+                after:top-1/2 after:z-0 after:flex after:items-center after:border-t"
+                >
+                  <span className="bg-card text-muted-foreground relative z-10 px-2 ">
+                    or continue with
+                  </span>
+                </div>
+                <div className="grid grid-cols-2 gap-4">
+                  <Button
+                    disabled={pending}
+                    variant="outline"
+                    type="button"
+                    className="w-full"
+                  >
+                    Google
+                  </Button>
+                  <Button
+                    disabled={pending}
+                    variant="outline"
+                    type="button"
+                    className="w-full"
+                  >
+                    Github
+                  </Button>
+                </div>
+                <div className="text-center text-sm">
+                  Aldready have an account?{" "}
+                  <Link
+                    href="/sign-in"
+                    className="underline underline-offset-4"
+                  >
+                    Sign In
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </Form>
+
+          <div className="bg-radial from-blue-600 to-blue-700 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+            <img src="/logo.svg" alt="Image" className="h-[104px] w-[92px]" />
+            <p className="text-2xl font-semibold text-white">Zap.AI</p>
+          </div>
+        </CardContent>
+      </Card>
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance  *:[a]:underline  *:[a]:underline-offset-4 ">
+        By Clicking continue, you agree to our <a href="#">Terms Of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>
+      </div>
+    </div>
+  );
 };
