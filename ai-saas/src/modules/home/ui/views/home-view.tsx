d5a5ca770ed584938c4c9169cec9ff33812ab130
APrem-7 <ap7leo@gmail.com>
2025-12-15 21:25:03 +0530

    Refactor: remove sign-up and login functionality from Home component and create HomeView server component for session management

--- ai-saas/src/modules/home/ui/views/home-view.tsx
+++ ai-saas/src/modules/home/ui/views/home-view.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 import { authClient } from "@/lib/auth-client";
 
-export default function Home() {
+export default function HomeView() {
   const { data: session } = authClient.useSession();
 
   if (!session) {
