7131b546998620efe96e443264aa44b7821707b1
APrem-7 <ap7leo@gmail.com>
2025-12-17 19:22:44 +0530

    feat: Added the second section in the siderbar

--- ai-saas/src/modules/dashboard/ui/components/dashboard-siderbar.tsx
+++ ai-saas/src/modules/dashboard/ui/components/dashboard-siderbar.tsx
@@ -42,11 +42,6 @@ const secondSection = [
     label: "Upgrade",
     href: "/upgrade",
   },
-  {
-    icon: BotIcon,
-    label: "Agents",
-    href: "/agents",
-  },
 ];
 
 export const DashboardSidebar = () => {
@@ -68,6 +63,37 @@ export const DashboardSidebar = () => {
           <SidebarGroupContent>
             <SidebarMenu>
               {firstSection.map((item) => (
+                <SidebarMenuItem key={item.href}>
+                  <SidebarMenuButton
+                    asChild
+                    className={cn(
+                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
+                      pathname === item.href &&
+                        "bg-linear-to-r/oklch border-[#5D6B68]/10"
+                    )}
+                    isActive={pathname === item.href}
+                  >
+                    <Link href={item.href}>
+                      <item.icon className="size-5"></item.icon>
+                      <span className="text-sm font-medium tracking-tight">
+                        {item.label}
+                      </span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        <div className="px-4 py-2">
+          <Separator className="opactity-10 text-[#5D6B68]" />
+        </div>
+
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {secondSection.map((item) => (
                 <SidebarMenuItem key={item.href}>
                   <SidebarMenuButton
                     asChild
