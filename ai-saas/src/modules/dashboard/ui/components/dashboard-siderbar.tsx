4e45f48d6851ac9d634a550761d52fc9940052f5
APrem-7 <ap7leo@gmail.com>
2025-12-17 19:18:11 +0530

    feat: integrate pathname for active state in DashboardSidebar

--- ai-saas/src/modules/dashboard/ui/components/dashboard-siderbar.tsx
+++ ai-saas/src/modules/dashboard/ui/components/dashboard-siderbar.tsx
@@ -21,6 +21,7 @@ import {
 
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 const firstSection = [
   {
@@ -49,6 +50,8 @@ const secondSection = [
 ];
 
 export const DashboardSidebar = () => {
+  const pathname = usePathname();
+  //   const pathname = "/agents";
   return (
     <Sidebar>
       <SidebarHeader className="text-sidebar-accent-foreground">
@@ -69,8 +72,11 @@ export const DashboardSidebar = () => {
                   <SidebarMenuButton
                     asChild
                     className={cn(
-                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50"
+                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
+                      pathname === item.href &&
+                        "bg-linear-to-r/oklch border-[#5D6B68]/10"
                     )}
+                    isActive={pathname === item.href}
                   >
                     <Link href={item.href}>
                       <item.icon className="size-5"></item.icon>
