d95b42fa7447cd9ae74ac49db873d42544ad2caa
APrem-7 <ap7leo@gmail.com>
2025-12-21 23:40:25 +0530

    feat: add DashboardCommand component and integrate with DashboardNavbar for command functionality

--- ai-saas/src/modules/dashboard/ui/components/dashboard-navbar.tsx
+++ ai-saas/src/modules/dashboard/ui/components/dashboard-navbar.tsx
@@ -3,12 +3,16 @@
 import { Button } from "@/components/ui/button";
 import { useSidebar } from "@/components/ui/sidebar";
 import { PanelLeftCloseIcon, PanelLeftIcon, SearchIcon } from "lucide-react";
+import { DashboardCommand } from "./dashboard-command";
+import { useState } from "react";
 
 export const DashboardNavbar = () => {
   const { state, toggleSidebar, isMobile } = useSidebar();
+  const [commandOpen, setCommandOpen] = useState(false);
 
   return (
     <>
+      <DashboardCommand open={commandOpen} setOpen={setCommandOpen} />
       <nav className="flex px-4 gap-x-2 items-center py-3 border-b bg-background">
         <Button className="size-9" variant="outline" onClick={toggleSidebar}>
           {state === "collapsed" || isMobile ? (
@@ -21,7 +25,9 @@ export const DashboardNavbar = () => {
           className="h-9 w-[240px] justify-start font-normal text-muted-foreground hover:text-muted-foreground"
           variant="outline"
           size="sm"
-          onClick={() => {}}
+          onClick={() => {
+            setCommandOpen((open) => !open);
+          }}
         >
           <SearchIcon />
           Search
