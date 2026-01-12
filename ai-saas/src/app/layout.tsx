62ce57525f32e79c1ec147bc60fc214d03d362a6
APrem-7 <ap7leo@gmail.com>
2026-01-12 17:10:08 +0530

    feat(layout): add NuqsAdapter wrapper for state management in layout of app

--- ai-saas/src/app/layout.tsx
+++ ai-saas/src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 
 import Providers from './providers';
 import { Toaster } from 'sonner';
+import {NuqsAdapter} from 'nuqs/adapters/next/app';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -23,8 +24,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <Providers>
-          <Toaster />
-          {children}
+          <NuqsAdapter>
+            <Toaster />
+            {children}
+          </NuqsAdapter>
         </Providers>
       </body>
     </html>
