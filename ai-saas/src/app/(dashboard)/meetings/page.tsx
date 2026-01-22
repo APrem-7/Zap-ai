3d9d53ddf622d724e5a6d136621aeb96db13bc8e
APrem-7 <ap7leo@gmail.com>
2026-01-22 23:41:44 +0530

    feat(page.tsx): add header component to meetings page
    Include MeetingsListHeader above the meeting view for better UI structure.

--- ai-saas/src/app/(dashboard)/meetings/page.tsx
+++ ai-saas/src/app/(dashboard)/meetings/page.tsx
@@ -1,10 +1,11 @@
 
 import {MeetingView} from "@/modules/meetings/ui/meetings-view";
+import {MeetingsListHeader} from "@/modules/meetings/components/meeting-list-header";
 
 const Page = () => {
   return (
     <>
-
+      <MeetingsListHeader />
       <div className="p-4 flex flex-col gap-y-4">
         <MeetingView />
       </div>
